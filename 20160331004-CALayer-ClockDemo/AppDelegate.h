f25c3119b6a80e1678b6f80e63c69558440ca65d
raineryue <it_jinlei.yue@163.com>
2016-03-31 22:19:34 +0800

    Initial Commit

---
+++ 20160331004-CALayer-ClockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160331004-CALayer-ClockDemo
+//
+//  Created by Rainer on 16/3/31.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
